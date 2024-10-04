a48629b0960eed00f4b52c9df7228be35229d668
tejasking62 <tejasriyer@gmail.com>
2024-10-04 18:05:56 -0400

    frontend

---
+++ my-app/src/signin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Leaders() {
+  return (
+    <div>
+      <h1>Sign In </h1>
+      <p>Meet the leaders who are making an impact in the nonprofit space.</p>
+    </div>
+  );
+}
+
+export default Leaders;
