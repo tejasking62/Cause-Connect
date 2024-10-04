125cd6f07fcf0ce31b94ef737aec45bb1fe53496
isoudine <idaher@wisc.edu>
2024-10-04 17:41:55 -0400

    skeleton code for landing pages

---
+++ my-app/src/Nonprofits.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Nonprofits() {
+  return (
+    <div>
+      <h1>Nonprofits Page</h1>
+      <p>Here you will find information about various nonprofit organizations.</p>
+    </div>
+  );
+}
+
+export default Nonprofits;
