d599380b5d9cbd21c7be032c7a0949197015b11f
isoudine <idaher@wisc.edu>
2024-10-04 17:30:23 -0400

    first skeleton react code

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+        <p>This is a simple skeleton for a React application.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
