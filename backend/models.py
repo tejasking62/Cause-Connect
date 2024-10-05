644723f37580e98dab7ed5cfa3c2cb0cfd9fa734
Ayo <ayooluwaadeleye17@gmail.com>
2024-10-05 03:31:03 -0400

    update models.py, added user.id key for nonprofit

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.sqlite import JSON
 
 db = SQLAlchemy()
 
@@ -8,23 +9,25 @@ class User(db.Model):
     username = db.Column(db.String(20), unique=True, nullable=False)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    role = db.Column(db.String(120), nullable=False, default='User')
     password = db.Column(db.String(60), nullable=False)
-
-    # Relationship to Nonprofits
-    nonprofits = db.relationship('Nonprofits', backref='author', lazy=True)
+    role = db.Column(db.String(20), nullable=False, default='User')
+    is_nonprofit = db.Column(db.Boolean, default=False)
+    data = db.Column(JSON)
+    
+    # Relationship to Nonprofits (if the user is associated with a nonprofit)
+    nonprofit = db.relationship('Nonprofit', backref='user', uselist=False)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.role}')"
 
-class Nonprofits(db.Model):
+class Nonprofit(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     title = db.Column(db.String(100), nullable=False)
     location = db.Column(db.String(100), nullable=False)
     sectors = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
-        return f"Nonprofits('{self.title}', '{self.date}')"
+        return f"Nonprofit('{self.title}', '{self.date}')"
