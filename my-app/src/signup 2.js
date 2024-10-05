ccea0f2a3512c680c8a80db047635e52e4db4cbc
shreyagavarapattu <sgavarap@umd.edu>
2024-10-05 07:58:24 -0400

    formatted the dashboard in order to include all user questions and proper formatting based on figma

---
+++ my-app/src/signup 2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Leaders() {
+  return (
+    <div>
+      <h1>Sign up </h1>
+      <p>Meet the leaders who are making an impact in the nonprofit space.</p>
+    </div>
+  );
+}
+
+export default Leaders;
