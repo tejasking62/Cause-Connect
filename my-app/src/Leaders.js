a6e1bdef0374b1d1513f543e39dbf4b841a5ae88
tejasking62 <tejasriyer@gmail.com>
2024-10-04 22:20:14 -0400

    l

--- my-app/src/Leaders.js
+++ my-app/src/Leaders.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 
 function Leaders() {
@@ -34,15 +33,6 @@ function Leaders() {
         </div>
         <button type="submit">Submit</button>
       </form>
-=======
-import React from 'react';
-
-function Leaders() {
-  return (
-    <div>
-      <h1>Leaders Page</h1>
-      <p>Meet the leaders who are making an impact in the nonprofit space.</p>
->>>>>>> origin/tejas
     </div>
   );
 }
